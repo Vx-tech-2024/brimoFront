cc96acc130982589e6ccba94ab4fd3334b029cc8
Vx-tech <xaviervianney8@gmail.com>
2026-04-09 00:25:03 +0300

    Errors

--- src/features/dashboard/dashboardApi.ts
+++ src/features/dashboard/dashboardApi.ts
@@ -1,7 +1,7 @@
 import axiosInstance from "../../lib/axios";
 
 const API = axiosInstance.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL ,
 });
 
 API.interceptors.request.use((config) => {
