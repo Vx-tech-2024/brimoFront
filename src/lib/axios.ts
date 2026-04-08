c890d1269944fb9fd71448c03be2e9cd71389576
Vx-tech <xaviervianney8@gmail.com>
2026-04-09 01:22:00 +0300

    dev tools usage

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
-
+console.log("API URL:", import.meta.env.VITE_API_BASE_URL);
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL ,
     withCredentials: true,
 });
+console.log("API URL:", import.meta.env.VITE_API_BASE_URL);
+
 
 let token: string | null = null;
 
