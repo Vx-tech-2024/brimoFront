8af021241534e4af7db62caa255712824aef622a
Vx-tech-2024 <joelgeorge6505@gmail.com>
2026-08-17 16:21:43 +0300

    Sidebar inclusion of daily activty

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -8,6 +8,7 @@ export default function Sidebar() {
     { name: "Agent Targets", path: "/target" },
     { name: "Performance", path: "/performance" },
     { name: "Reports", path: "/reports" },
+    { name: "Daily Activity", path: "/daily-activity" },
   ];
 
   return (
